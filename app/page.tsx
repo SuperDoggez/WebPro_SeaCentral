08f7932fa418c263625899dfca9e60065d730c80
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-16 18:40:25 +0700

    origin

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <img src="https://dfmtboqfsygnjttfuvgq.supabase.co/storage/v1/object/public/b-trade/profile/43474571-17aa-4c70-bf6c-960848a25ed4.jpg" alt="" />
+  );
+}
---
+++ lib/auth.ts
@@ -0,0 +1,76 @@
+import { type NextAuthOptions } from 'next-auth';
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prismadb } from "./db";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+    pages: {
+        signIn: '/login'
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+      strategy: 'jwt',
+      maxAge: 30 * 24 * 60 * 60, 
+      updateAge: 30 * 24 * 60 * 60
+    },
+    providers: [
+        CredentialsProvider({
+          name: "Credentials",
+          credentials: {
+            email: { label: "Email", type: "email"},
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials) {
+            if (!credentials?.email || !credentials?.password) {
+              return null
+            }
+            
+            const user = await prismadb.user.findUnique({           
+                where: { email: credentials.email }         
+            })
+            if (!user) {
+                throw new Error('Invalid email')
+            }
+            const matchpassword = await compare(credentials.password, user.password)
+
+            if ( user && matchpassword ) {
+              return {
+                id: `${user.id}`,
+                username: user.username,
+                email: user.email,
+                isAdmin: user.isAdmin
+              }
+            } else {
+              throw new Error('Invalid password')
+            }
+            
+          }
+        })
+      ], 
+      callbacks: {
+        jwt: ({ token, user }) => {
+          if (user) {
+            return {
+              username: user.username,
+              id: user.id,
+              isAdmin: user.isAdmin,
+              ...token
+            }
+          }
+          return token
+        },
+        session: ({ session, token }) => {
+          return {
+            ...session,
+            user: {
+              ...session.user,
+              id: token.id,
+              username: token.username,
+              isAdmin: token.isAdmin
+            }
+          }
+        },
+        
+      }
+
+}

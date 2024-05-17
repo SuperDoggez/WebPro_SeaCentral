b9ff1f7244801734fbe374f6b2de6843a0dcd345
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-17 18:05:26 +0700

    booking

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -2,14 +2,11 @@ import { NextResponse } from "next/server";
 import { prismadb } from "@/lib/db";
 
 export async function POST(req: Request) {
-    function uniqueID() {
-        return Math.floor(Math.random() * 3)
-        }
     
     try {
-        const id = uniqueID()
+        const id = Math.floor(Math.random() * Date.now())
         
-        const { first_name, last_name, email, phone_number, phone_number_2, description, total_price, country, for_other, checkin, checkout} = await req.json()
+        const { first_name, last_name, email, phone_number, phone_number_2, description, total_price, country, for_other, checkin, checkout, room_type_id, activity_id, package_id} = await req.json()
 
         const checkin_date = new Date(checkin)
         const checkout_date = new Date(checkout)
@@ -37,18 +34,54 @@ export async function POST(req: Request) {
                 tourist_id:newTourist.id
             }
         })
-        
+
+        if (room_type_id) {
+            for (let i = 0;i < room_type_id.length; i++) {
+            await prismadb.book_room.create({
+                data: { 
+                    book_id:id,
+                    room_type_id:room_type_id[i],
+                    status: 'pending'
+                }
+            })
+            }
+        }
+
+        if (activity_id) {
+            for (let i = 0;i < activity_id.length; i++) {
+            await prismadb.book_activity.create({
+                data: { 
+                    book_id:id,
+                    activity_id:activity_id[i],
+                    status: 'pending'
+                }
+            })
+            }
+        }
+
+        if (package_id) {
+            for (let i = 0;i < package_id.length; i++) {
+                await prismadb.book_package.create({
+                    data: { 
+                        book_id:id,
+                        package_id:package_id[i],
+                        status: 'pending'
+                    }
+                })
+                }
+        }
+
         return NextResponse.json({
-            booking:newBooking,
+            booking:{...newBooking,id:newBooking.id.toString()},
             tourist:newTourist,
             message: "Book room succesfully"
         }, { status: 201 })
     
-        
+        return NextResponse.json({message:"hi"})
     } catch (error) {
         
         console.log(error);
-        NextResponse.json({error})
+        return NextResponse.json({error},{status:404})
         
     }
 }

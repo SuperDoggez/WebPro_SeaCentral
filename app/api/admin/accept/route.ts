b8f6200498eb6577e85ef26d19c1bfd96dc33dc2
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-05-21 15:54:59 +0700

    admin editinfo

---
+++ app/api/admin/accept/route.ts
@@ -0,0 +1,78 @@
+import { NextResponse } from "next/server";
+import { prismadb } from "@/lib/db";
+
+export async function PUT(req: Request) {
+    try {
+        const { id } = await req.json()
+
+        const find_booking = await prismadb.booking.findUnique({
+            where: { id:id },
+            select: {
+                Book_room:true,
+                Book_activity:true,
+                Book_package:true
+            }
+        })
+
+        if (!find_booking) {
+            return NextResponse.json({
+                booking:null,
+                message: "Booking not found"
+            })
+        }
+
+        const booking = await prismadb.booking.update({
+            where: { id:id },
+            data: {
+                status: 'accept'
+            }
+        })
+
+        if (find_booking.Book_room.length > 0) {
+            const room = await prismadb.book_room.updateMany({
+                where: { book_id:id },
+                data: {
+                    status: 'accept'
+                }
+            })
+            if (find_booking.Book_activity.length > 0) {
+                const activity = await prismadb.book_activity.updateMany({
+                    where: { book_id:id },
+                    data: {
+                        status: 'accept'
+                    }
+                })
+                return NextResponse.json({
+                    booking:booking,
+                    room:room,
+                    activity:activity,
+                    message: "Book room and activity request have been accepted"
+                })
+            }
+            return NextResponse.json({
+                booking:booking,
+                room:room,
+                message: "Book room request have been accepted"
+            })
+        } else {
+            const package_ = await prismadb.book_package.updateMany({
+                where: { book_id:id },
+                data: {
+                    status: 'accept'
+                }
+            })
+
+            return NextResponse.json({
+                package:package_,
+                message: "Book package request have been accepted"
+            })
+        }
+
+
+    } catch (error) {
+        
+        console.log(error);
+        NextResponse.json({error})
+
+    }
+}
---
+++ app/api/admin/editinfo/room/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import { prismadb } from "@/lib/db";
+import exp from "constants";
+
+export async function PUT(req: Request) {
+    try {
+        const { id, price, swimming_pool, bath_tub} = await req.json()
+        
+        const newRoom = await prismadb.room_type.update({
+            where: { id:id },
+            data: {
+                price,
+                swimming_pool,
+                bath_tub
+            }
+        })
+
+        return NextResponse.json({
+            Room:newRoom,
+            message: "Room information have been changed"
+        })
+
+
+    } catch (error) {
+
+        console.log(error);
+        return NextResponse.json({error})
+    
+    }
+}
+
+export async function GET(req: Request) {
+    try {
+        const allroom = await prismadb.room_type.findMany({
+            select: {
+                name:true,
+                price:true,
+                picture:true,
+                swimming_pool:true,
+                bath_tub:true
+            }
+        })
+
+        return NextResponse.json({
+            room:allroom,
+            message: "All Room information have been sent succesfully"
+        }) 
+
+    } catch (error) {
+
+        console.log(error);
+        return NextResponse.json({error})     
+    }
+   
+}

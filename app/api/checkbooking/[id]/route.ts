10f7ecd3cb74dea96062e33a24fca36ceff411c3
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2024-06-06 19:10:52 +0700

    checkbooking

--- app/api/checkbooking/[id]/route.ts
+++ app/api/checkbooking/[id]/route.ts
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server";
 import { prismadb } from "@/lib/db";
 
+interface Room {
+    id: number,
+    name: string,
+    price: number
+}
+
 interface Activity {
     id: number,
     name: string,
@@ -9,15 +15,13 @@ interface Activity {
 
 interface Package {
     id: number,
+    name: string,
     price: number,
-    room_type: number,
-    activity: number
 }
 
-interface Room {
+interface Count {
     id: number,
-    name: string,
-    price: number
+    count: number
 }
 
 export async function GET(req: Request,{ params } : { params : { id: string }}) {
@@ -25,20 +29,19 @@ export async function GET(req: Request,{ params } : { params : { id: string }}) 
         const booking = await prismadb.booking.findUnique({
             where: { id:parseInt(params.id)},
             select: {
-                tourist_info:true,
-                Book_activity: {
+                status:true,
+                checkin:true,
+                checkout:true,
+                children:true,
+                adult:true,
+                description:true,
+                tourist_info: {
                     select: {
-                        activity_id:true
-                    }
-                },
-                Book_package: {
-                    select: {
-                        package_id:true
-                    }
-                },
-                Book_room: {
-                    select: {
-                        room_type_id:true
+                        id:true,
+                        first_name:true,
+                        last_name:true,
+                        phone_number:true,
+                        phone_number_2:true
                     }
                 }
             }
@@ -52,6 +55,7 @@ export async function GET(req: Request,{ params } : { params : { id: string }}) 
         }
 
         const room:Room[] = []
+        let roomcount: Count[] = []
         const room_type = await prismadb.book_room.findMany({
             where: { book_id:parseInt(params.id) },
         })
@@ -60,8 +64,9 @@ export async function GET(req: Request,{ params } : { params : { id: string }}) 
             const rm:number[] = []
             const uniqueid = room_type.map(id => id.room_type_id).filter((id, index, self) => self.indexOf(id) === index)
             rm.push(...uniqueid)
+
             for (let i = 0;i < rm.length;i++) {
-                const rooms = await prismadb.room_type.findMany({
+                const room_info = await prismadb.room_type.findMany({
                     where: { id:rm[i] },
                     select: {
                         id:true,
@@ -69,21 +74,30 @@ export async function GET(req: Request,{ params } : { params : { id: string }}) 
                         price:true 
                     }
                 })
-                room.push(...rooms)
+                room.push(...room_info)
             }
+            const room_id = room_type.map(room => room.room_type_id);
+            const uniqueroom_id = [...new Set(room_id)];
+            roomcount = uniqueroom_id.map(id => ({
+                id,
+                count: room_id.filter(roomid => roomid === id).length
+            }));
         }
 
         let activity:Activity[] = []
+        let activitycount:Count[] = []
+
         const activities = await prismadb.book_activity.findMany({
             where: { book_id:parseInt(params.id) },
         })
 
         if (activities) {
-            const ac:number[] = []
+            const ac: number[] = []
             const uniqueid = activities.map(id => id.activity_id).filter((id, index, self) => self.indexOf(id) === index)
             ac.push(...uniqueid)
+
             for (let i = 0;i < ac.length;i++) {
-                const all_activity = await prismadb.activity.findMany({
+                const activity_info = await prismadb.activity.findMany({
                     where: { id:ac[i] },
                     select: {
                         id:true,
@@ -91,36 +105,53 @@ export async function GET(req: Request,{ params } : { params : { id: string }}) 
                         discount_price:true   
                     }
                 })
-                activity.push(...all_activity)
+                activity.push(...activity_info)
             }
+            const activity_id = activities.map(id => id.activity_id)
+            const uniqueactivityid = [... new Set(activity_id)]
+            activitycount = uniqueactivityid.map(id => ({
+                id,
+                count: activity_id.filter(activityid => activityid = id).length
+            }))
         }
 
-        let package_:Package[] = []
-        // const packages = await prismadb.book_package.findMany({
-        //     where: { book_id:parseInt(params.id) },
-        // })
+        let package_: Package[] = []
+        let packagecount: Count[] = []
 
-        // if (packages) {
-        //     const pc:number[] = []
-        //     const uniqueid = packages.map(id => id.package_id).filter((id, index, self) => self.indexOf(id) === index)
-        //     pc.push(...uniqueid)
-        //     for (let i = 0;i < pc.length;i++) {
-        //         package_ = await prismadb.package.findMany({
-        //             where: { id:pc[i] },
-        //             select: {
-        //                 id:true,
-        //                 room_type:true,
-        //                 activity:true,
-        //                 price:true  
-        //             }
-        //         })
-        //     }
-        // }
+        const packages = await prismadb.book_package.findMany({
+            where: { book_id:parseInt(params.id) },
+        })
+        
+        if (packages) {
+            const pc:number[] = []
+            const uniqueid = packages.map(id => id.package_id).filter((id, index, self) => self.indexOf(id) === index)
+            pc.push(...uniqueid)
+
+            for (let i = 0;i < pc.length;i++) {
+                const package_info = await prismadb.package.findMany({
+                    where: { id:pc[i] },
+                    select: {
+                        id:true,
+                        name:true,
+                        price:true  
+                    }
+                })
+                package_.push(...package_info)
+            }
+            const package_id = packages.map(packagep => packagep.package_id);
+            const uniquepackage_id = [...new Set(package_id)];
+            packagecount = uniquepackage_id.map(id => ({
+                id,
+                count: package_id.filter(packageid => packageid === id).length
+            }));
+        }
         
 
         return NextResponse.json({
-            booking:booking,
-            activity,room,package_,
+            booking,
+            room,roomcount,
+            activity,activitycount,
+            package_,packagecount,
             message: "Booking information has been sent successfully."
         })
         
